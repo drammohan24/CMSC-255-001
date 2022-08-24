d7d2e1e728c4a86546fa7e329bd4a06f90029ce1
drammohan24 <111944878+drammohan24@users.noreply.github.com>
2022-08-24 16:05:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/********************************************************************************************
+* Hello Lab Program
+*********************************************************************************************
+* Diya Ram Mohan
+* 8/24/2022
+* CMSC-255-001
+*********************************************************************************************/
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Diya Ram Mohan"); 
+		System.out.println("Hello Lab Program"); 
+		System.out.println("This lab is to ensure that java has been accuratly installed into our computers and get practice with the basics of running java code."); 
+		System.out.println("8/24/2022"); 
+		System.out.println("CMSC-255-001");
+		System.out.println();
+	}
+}
